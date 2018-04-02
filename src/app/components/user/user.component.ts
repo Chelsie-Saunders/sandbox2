4c8e8d874e8c5c0a6e496a8509cff351b9e767d1
Chelsie Saunders <chelsie.saunders@plusonecompany.info>
2018-04-02 17:20:43 -0600

    Learned how to loop with ngFor

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { User } from '../../models/User';
 @Component({
@@ -7,22 +7,23 @@ import { User } from '../../models/User';
   styleUrls: ['./user.component.css']
 })
 
-export class UserComponent {
+export class UserComponent implements OnInit{
   //Properties
     user: User;
 //Methods
   constructor() {
-      this.user = {
-        firstName: 'Chelsie',
-        lastName: 'Saunders',
-        age : 41,
-        address: {
-          street: '242 Spencer Peak Way G12',
-          city: 'Draper',
-          state: 'UT'
-        }
+  }
+  ngOnInit(){
+    this.user = {
+      firstName: 'Chelsie',
+      lastName: 'Saunders',
+      age : 41,
+      address: {
+        street: '242 Spencer Peak Way G12',
+        city: 'Draper',
+        state: 'UT'
       }
-
+    }
   }
 
 }
--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,15 +1,74 @@
 import { Component, OnInit } from '@angular/core';
 
+import { User } from '../../models/User';
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
+  users: User[];
 
   constructor() { }
 
   ngOnInit() {
-  }
+    this.users = [
+      {
+        firstName: 'Chelsie',
+        lastName: 'Saunders',
+        age : 41,
+        address: {
+          street: '242 Spencer Peak Way G12',
+          city: 'Draper',
+          state: 'UT'
+        }
+      },
+      {
+        firstName: 'Stuart Mac',
+        lastName: 'Saunders',
+        age : 56,
+        address: {
+          street: '242 Spencer Peak Way G12',
+          city: 'Draper',
+          state: 'UT'
+        }
+      },
+      {
+        firstName: 'Gabby',
+        lastName: 'Saunders',
+        age : 21,
+        address: {
+          street: '242 Spencer Peak Way G12',
+          city: 'Draper',
+          state: 'UT'
+        }
+      },
+      {
+        firstName: 'Duncan',
+        lastName: 'Saunders',
+        age : 19,
+        address: {
+          street: '242 Spencer Peak Way G12',
+          city: 'Draper',
+          state: 'UT'
+        }
+      }
 
+    ];
+
+    this.addUser({
+      firstName: 'Pepper',
+      lastName: 'Saunders',
+      age : 12,
+      address: {
+        street: '242 Spencer Peak Way G12',
+        city: 'Draper',
+        state: 'UT'
+      }
+    });
+  }
+  addUser(user: User){
+    this.users.push(user);
+
+  }
 }
